13dff8bebe0a06a7b8e569f8d2b2f8fe60e63447
bj0key <95365351+bj0key@users.noreply.github.com>
2025-12-08 23:01:42 +0000

    cleanup of day 8 solutions

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -16,16 +16,19 @@ fn parse(input: &str) -> Vec<Junction> {
     junctions
 }
 
-fn precalc_idx_combos(len: usize) -> Vec<(u16, u16)> {
+fn precalc_idx_combos(junctions: &[Junction]) -> Vec<(u16, u16)> {
     // save a bit of space by working in u16s,
     // we can cast them back to usizes later
-    let len_u16: u16 = len.try_into().unwrap();
+    let len = junctions.len();
     let mut v = Vec::with_capacity((len * (len - 1)) / 2);
+
+    let len_u16: u16 = len.try_into().unwrap();
     for i in 1..len_u16 {
         for j in 0..i {
             v.push((i, j));
         }
     }
+    v.sort_by_key(|&(i, j)| distance(&junctions[i as usize], &junctions[j as usize]));
     v
 }
 
@@ -52,13 +55,10 @@ fn all_merged(circuits: &[u16]) -> bool {
 }
 
 fn part1(junctions: &[Junction]) -> u64 {
-    let idx_combos = {
-        let mut combos = precalc_idx_combos(junctions.len());
-        combos.sort_by_cached_key(|(i, j)| {
-            distance(&junctions[*i as usize], &junctions[*j as usize])
-        });
-        combos
-    };
+    let idx_combos = precalc_idx_combos(junctions);
+    
+    // circuits[i] represents which circuit junctions[i] is currently on
+    // at first every junction has its own circuit
     let mut circuits: Vec<u16> = (0..junctions.len().try_into().unwrap()).collect();
     for i in 0..1000 {
         let [cx, cy] = <[_; 2]>::from(idx_combos[i]).map(|n| circuits[n as usize]);
@@ -77,14 +77,12 @@ fn part1(junctions: &[Junction]) -> u64 {
 }
 
 fn part2(junctions: &[Junction]) -> u64 {
-    let idx_combos = {
-        let mut combos = precalc_idx_combos(junctions.len());
-        combos.sort_by_cached_key(|(i, j)| {
-            distance(&junctions[*i as usize], &junctions[*j as usize])
-        });
-        combos
-    };
+    // Part 1, except instead of ending after 1000 iters & multiplying 3,
+    // we just keep going until sorted & multiply the final paring's X coords
+
+    let idx_combos = precalc_idx_combos(junctions);
     let mut circuits: Vec<u16> = (0..junctions.len().try_into().unwrap()).collect();
+
     for combo in idx_combos.iter() {
         let [x, y] = <[_; 2]>::from(*combo).map(|n| n as usize);
         let [cx, cy] = [circuits[x], circuits[y]];
